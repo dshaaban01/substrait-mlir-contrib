d0646a498a8d3049c6f1294b73749f29022efb64
Dalia Shaaban <144673861+dshaaban01@users.noreply.github.com>
2025-01-16 15:39:44 +0100

    clang gross

--- lib/Target/SubstraitPB/Export.cpp
+++ lib/Target/SubstraitPB/Export.cpp
@@ -666,11 +666,11 @@ SubstraitExporter::exportOperation(LiteralOp op) {
     literal->set_timestamp(value.cast<TimestampAttr>().getValue());
   } else if (literalType.isa<TimestampTzType>()) {
     literal->set_timestamp_tz(value.cast<TimestampTzAttr>().getValue());
-  } 
+  }
   // `DateType`.
   else if (auto binaryType = dyn_cast<DateType>(literalType)) {
     literal->set_date(value.cast<DateAttr>().getValue());
-  } 
+  }
   // `TimeType`.
   else if (literalType.isa<TimeType>()) {
     literal->set_time(value.cast<TimeAttr>().getValue());
